0b2fb807411ca86eb392acf8682171adbdaf2164
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-06-24 18:12:44 +0300

    project initiate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+    <h1 className='text-red-500 shadow-lg p-3 rounded-lg hover:shadow-2xl duration-500'>Hello World!</h1>    
+    
+    </>
+  )
+}
+
+export default App
